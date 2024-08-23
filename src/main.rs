87ea68088f2bcc7846f1a6777d9dd2ea1080fd8c
Code pianist <77950104+1739616529@users.noreply.github.com>
2024-08-23 15:32:46 +0800

    feat: 1

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 
 fn main() {
     
-    #[cfg(windows)]
-    println!("this is windows platform");
+    #[cfg(all(target_os = "windows", target_arch = "x86_64"))]
+    println!("this is windows x64 platform");
+    #[cfg(all(target_os = "windows", target_arch = "x86"))]
+    println!("this is windows x86 platform");
+    #[cfg(all(target_os = "windows", target_arch = "aarch64"))]
+    println!("this is windows aarch64 platform");
 
-    #[cfg(target_os = "macos")]
-    println!("this is macos platform");
+    #[cfg(all(target_os = "macos", target_arch = "x86_64"))]
+    println!("this is macos x64 platform");
+    #[cfg(all(target_os = "macos", target_arch = "x86"))]
+    println!("this is macos x86 platform");
+    #[cfg(all(target_os = "macos", target_arch = "aarch64"))]
+    println!("this is macos aarch64 platform");
 
-    
-    #[cfg(target_os = "linux")]
-    println!("this is macos platform");
+    #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
+    println!("this is linux x64 platform");
+    #[cfg(all(target_os = "linux", target_arch = "x86"))]
+    println!("this is linux x86 platform");
+    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+    println!("this is linux aarch64 platform");
+
 
 }
